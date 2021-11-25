7bdbe1445df4e681ee683e2c18cf1037e4c83433
alecngai <alecngai.jobs@gmail.com>
2021-11-25 14:49:29 -0500

    Added imgs and finished

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #perform summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
+
+#Deliverable 2------------------------------------------------------------
+#read file
+Suspension <- read.csv(file='Resources/Suspension_coil.csv',check.names=F,stringsAsFactors = F)
+
+#get a total summary
+total_summary <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI)) 
+
+#create a lot summary
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+
+lot_summary
